6f0015a3fdeaf9c41b722ec16e91c3d9d86154e1
NIKHIL <116481885+NikhilShetty-45@users.noreply.github.com>
2025-05-09 00:18:54 +0530

    Adding and Styling Navigation Bar

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,6 +1,7 @@
-import { List, ListItem, ListItemText, Typography } from "@mui/material";
+import { Container, CssBaseline, List, ListItem, ListItemText} from "@mui/material";
 import axios from "axios";
 import {useEffect, useState } from "react"
+import NavBar from "./NavBar";
 
 function App() {
   const [activities,setActivities] = useState<Activity[]>([]);
@@ -13,14 +14,18 @@ function App() {
 
   return (
     <>
-      <Typography variant="h3">Reactivities</Typography>
-      <List>
+    <CssBaseline/>
+      <NavBar/>
+      <Container maxWidth='xl' sx={{mt: 3}}>
+        <List>
         {activities.map((activity) =>(
           <ListItem key={activity.id}>
             <ListItemText>{activity.title}</ListItemText>
           </ListItem>
         ))}
       </List>
+      </Container>
+      
     </>
   )
 }
